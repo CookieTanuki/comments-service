e0dd99c27c1baaa8a7df47736e84e49853ca34d8
Nikita Demchenko <nikitosi3012@gmail.com>
2026-03-18 00:27:42 +0200

    add reply endpoint

--- backend/apps/comments/urls.py
+++ backend/apps/comments/urls.py
@@ -3,4 +3,6 @@ from .views import CommentReplyView, CommentListView
 
 urlpatterns = [
     path('', CommentListView.as_view(), name='comments'),
+    path('<int:pk>/reply/', CommentReplyView.as_view(), name='comment-reply'),
+
 ]
