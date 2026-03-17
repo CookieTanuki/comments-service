97873acbf23acb26d57bb7ee6379cc31517229f8
Nikita Demchenko <nikitosi3012@gmail.com>
2026-03-18 01:00:19 +0200

    add comment preview endpoint

--- backend/apps/comments/urls.py
+++ backend/apps/comments/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import CommentReplyView, CommentListView
+from .views import CommentReplyView, CommentListView, CommentPreviewView
 
 urlpatterns = [
     path('', CommentListView.as_view(), name='comments'),
     path('<int:pk>/reply/', CommentReplyView.as_view(), name='comment-reply'),
-
+    path('preview/', CommentPreviewView.as_view(), name='comment-preview'),
 ]
