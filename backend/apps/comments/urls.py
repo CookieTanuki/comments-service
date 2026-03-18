f024bf7d897164edcf3ae6f2c70c8f477f61a321
Nikita Demchenko <nikitosi3012@gmail.com>
2026-03-18 05:45:38 +0200

    refactor using namespace

--- backend/apps/comments/urls.py
+++ backend/apps/comments/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import CommentReplyView, CommentListView, CommentPreviewView
 
+
+app_name = 'comments'
+
 urlpatterns = [
-    path('', CommentListView.as_view(), name='comments'),
+    path('', CommentListView.as_view(), name='list'),
     path('<int:pk>/reply/', CommentReplyView.as_view(), name='comment-reply'),
     path('preview/', CommentPreviewView.as_view(), name='comment-preview'),
 ]
