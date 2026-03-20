80aa2ff2194d69f059402c51d88bf7184474b197
Nikita Demchenko <nikitosi3012@gmail.com>
2026-03-20 18:11:43 +0200

    add signal decreasing replies count

--- backend/apps/comments/signals.py
+++ backend/apps/comments/signals.py
@@ -1,11 +1,18 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from .models import Comment
 
 @receiver(post_save, sender=Comment)
-def update_parent_replies_count(sender, instance, created, **kwargs):
+def update_parent_replies_count_on_create(sender, instance, created, **kwargs):
     if created and instance.parent:
         parent = instance.parent
         parent.replies_count += 1
+        parent.save(update_fields=["replies_count"])
+
+@receiver(post_delete, sender=Comment)
+def update_replies_count_on_delete(sender, instance, **kwargs):
+    if instance.parent:
+        parent = instance.parent
+        parent.replies_count -= 1
         parent.save(update_fields=["replies_count"])
