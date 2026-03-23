4311fd4275e322fb3cc1001793cc87ed6045a206
Nikita Demchenko <nikitosi3012@gmail.com>
2026-03-23 17:57:06 +0200

    hotfix typo

--- backend/apps/comments/routing.py
+++ backend/apps/comments/routing.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .consumers import CommentConsumer
+from .consumers import CommentsConsumer
 
 
 websocket_urlpatterns = [
-    path('ws/comments/', CommentConsumer.as_asgi()),
+    path("ws/comments/", CommentsConsumer.as_asgi()),
 ]
