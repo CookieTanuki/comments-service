81d6969d682868a0e0451c4bbe8192881e24df08
Nikita Demchenko <nikitosi3012@gmail.com>
2026-03-23 18:21:13 +0200

    update settings (channel-redis) for prod

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -1,3 +1,5 @@
+import os
+
 from .base import *
 
 DEBUG = False
@@ -8,3 +10,17 @@ USE_S3 = False
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [
+                os.getenv(
+                    "CHANNEL_LAYERS_REDIS_URL",
+                    os.getenv("REDIS_URL", "redis://redis:6379/2"),
+                )
+            ],
+        },
+    }
+}
