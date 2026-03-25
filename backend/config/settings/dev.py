1253773d60630969ba24e8aeee11fed70f902112
Nikita Demchenko <nikitosi3012@gmail.com>
2026-03-25 12:22:19 +0200

    update settings

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -3,10 +3,26 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
 
-REST_FRAMEWORK["DEFAULT_AUTHENTICATION_CLASSES"] = ["rest_framework.authentication.SessionAuthentication"]
+dev_cache_url = os.getenv("DEV_CACHE_URL")
 
-CACHES["default"]["LOCATION"] = "redis://127.0.0.1:6379/1"
+if dev_cache_url:
+    CACHES["default"]["LOCATION"] = dev_cache_url
+else:
+    CACHES["default"] = {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "comments-service-dev",
+    }
 
 USE_S3 = False
 
