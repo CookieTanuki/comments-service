81b7a44df9907bd9155780dcb7afb2a89bfc6263
Nikita Demchenko <nikitosi3012@gmail.com>
2026-03-25 12:28:58 +0200

    configuring settings

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -15,7 +15,7 @@ from channels.auth import AuthMiddlewareStack
 
 import apps.comments.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
 
 django_asgi_app = get_asgi_application()
 
